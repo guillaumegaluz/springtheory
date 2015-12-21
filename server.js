eccebcc108b04b1506789c6145a9a905b4f8a490
Guillaume Galuz <guillaume@uber.com>
2015-12-20 22:46:55 -0800

    change server port in production

--- server.js
+++ server.js
@@ -2,14 +2,17 @@ var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
 
+var serverPort = process.env.PORT || 3000;
+var serverHost = '0.0.0.0';
+
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
+}).listen(serverPort, serverHost, function (err, result) {
   if (err) {
     console.log(err);
   }
 
-  console.log('Listening at localhost:3000');
+  console.log("Listening at " serverHost + ":" + serverPort);
 });
