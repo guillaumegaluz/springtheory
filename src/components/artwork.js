77ecb8f2d3325aa5099730483503b8c798532641
Guillaume Galuz <guillaume.galuz@gmail.com>
2020-04-19 18:22:39 -0700

    create a new release

--- src/components/artwork.js
+++ src/components/artwork.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react'
 
-import mobile from 'is-mobile';
+import mobile from 'is-mobile'
 
 const Artwork = function(props) {
   const {
